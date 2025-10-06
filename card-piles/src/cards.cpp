14f2e540962a63d32b3ca3abca0d37335f8d3e73
samui. <baleevamasha64@gmail.com>
2025-10-06 22:49:36 +0300

    Update cards.cpp

--- card-piles/src/cards.cpp
+++ card-piles/src/cards.cpp
@@ -1,3 +1,11 @@
 #include <cards.h>
 #include <algorithm>
 #include <random>
+
+std::string Card::toString() const {
+    static const std::string suitNames[] = {"♠", "♥", "♦", "♣"};
+    static const std::string valueNames[] =
+    {
+        "", "A", "2", "3", "4", "5", "6", "7",
+        "8", "9", "10", "J", "Q", "K"
+    };
