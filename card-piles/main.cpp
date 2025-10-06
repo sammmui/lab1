b8b95de912de44162a75857a5c146cafe6f2fc6d
Albina Kara <kara_albina@knu.ua>
2025-10-06 23:01:11 +0300

    create main

--- card-piles/main.cpp
+++ card-piles/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include <stdexcept>
-
+#include <limits>
 #include "cards.h"
 #include "stack.h"
 
+using namespace std;
+
 int main() {
+    int suits;
+    cout << "Enter number of suits: ";
+    cin >> suits;
+    if (cin.fail() || suits <= 0) {
+        cerr << "Error: Invalid number of suits (must be a positive integer)\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return 1;
+    }
+
+    int num_cards;
+    cout << "Enter number of cards to deal: ";
+    cin >> num_cards;
+    if (cin.fail() || num_cards < 0) {
+        cerr << "Error: Invalid number of cards (must be a non-negative integer)\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return 1;
+    }
+
     
-}
