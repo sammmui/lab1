797240873ef9da34a6ab5d17020908916f1810eb
Albina Kara <kara_albina@knu.ua>
2025-10-06 17:18:38 +0300

    test-commit

---
+++ include/stack.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string> 
+
+class Stats {
+    private:
+    std:vector<int> pile_lenghts;
+
+    public:
+    void addPile(int lenght);
+    double mean() const;
+    double median() const;
+    int mode() const;
+    std:map<int, double> percentages() const;
+    void printReport() const;
+};
